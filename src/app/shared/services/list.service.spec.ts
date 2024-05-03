31d884eb7d03400f0dc684586f34ae920d8562f9
Maksym Niekrasov <maksym_niekrasov@epam.com>
2024-05-03 12:10:33 +0300

    fix UT

--- src/app/shared/services/list.service.spec.ts
+++ src/app/shared/services/list.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListService } from './list.service';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
 describe('ListService', () => {
   let service: ListService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ListService]
+    });
     service = TestBed.inject(ListService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
